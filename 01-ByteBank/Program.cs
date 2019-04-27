dbfd6ea8e5060d5aea94592b79d0310c2a237e41
Victor Rodrigues dos Santos <victor.rsantos@outlook.com>
2019-04-27 06:53:47 -0300

    commit

---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela =  new ContaCorrente();
+
+
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+            contaDaGabriela.saldo = 100;
+
+
+            Console.WriteLine("Nome: {0}",contaDaGabriela.titular);
+            Console.WriteLine("Agência: {0}", contaDaGabriela.agencia);
+            Console.WriteLine("Número: {0}",contaDaGabriela.numero);
+            Console.WriteLine("Saldo: {0}",contaDaGabriela.saldo);
+
+            contaDaGabriela.saldo += 200;
+
+            Console.WriteLine("Saldo: {0}", contaDaGabriela.saldo);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ContaCorrente contaGabriela = new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 863;
+            contaGabriela.numero = 863452;
+
+
+
+
+        }
+    }
+}
