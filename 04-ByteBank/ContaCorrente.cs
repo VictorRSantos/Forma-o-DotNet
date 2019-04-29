3eabb1e51af11b0430e2011dba7e4fbfb4a3f41b
Victor Rodrigues dos Santos <victor.rsantos@outlook.com>
2019-04-29 08:08:37 -0300

    Entendendo namespaces

--- 04-ByteBank/ContaCorrente.cs
+++ 04-ByteBank/ContaCorrente.cs
@@ -1,62 +1,63 @@
-﻿
+﻿using _05_ByteBank;
 
-
-public class ContaCorrente
+namespace _05_ByteBank
 {
-
-    public string titular;
-    public int agencia;
-    public int numero;
-    public double saldo = 100;
-
-
-    public bool Sacar(double valor)
+    public class ContaCorrente
     {
 
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo = 100;
 
-        if (this.saldo < valor)
+
+        public bool Sacar(double valor)
         {
-            return false;
+
+
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+
+
         }
-        else
+
+
+        public void Depositar(double valor)
         {
+
+            this.saldo += valor;
+
+        }
+
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+
+
+            if (this.saldo < valor)
+            {
+                return false;
+
+            }
+
             this.saldo -= valor;
+
+            contaDestino.Depositar(valor);
+
             return true;
+
+
         }
 
-        
-    }
-    
-
-    public void Depositar(double valor)
-    {
-
-        this.saldo += valor;
 
     }
-
-
-    public bool Transferir(double valor, ContaCorrente contaDestino)
-    {
-
-
-        if (this.saldo < valor)
-        {
-        return false;
-
-        }
-       
-        this.saldo -= valor;
-
-        contaDestino.Depositar(valor);
-
-        return true;
-
-
-
-
-
-    }
-
 
 }
+
